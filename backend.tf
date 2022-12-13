693ff1c7f802c911be49431c8bf773fb3546dbb7
Pratima <116800696+yadav1705@users.noreply.github.com>
2022-12-13 17:24:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "my-dev-bucket-1000"
     key = "main"
-    region = "ap-south-1"
+    region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
